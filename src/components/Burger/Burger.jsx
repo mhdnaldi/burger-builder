25b7aea5223691a94fc3f09916ff8d129a3814d2
mhdnaldi <mhd.naldi@yahoo.com>
2020-12-23 22:18:07 +0700

    add styling

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,7 +3,7 @@ import classes from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 // Object.keys() convert object keys into an array
-
+  
 const Burger = props => {
   let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
