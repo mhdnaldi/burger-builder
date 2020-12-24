67b714c66bd42b17f3599579190048de596a0e64
mhdnaldi <mhd.naldi@yahoo.com>
2020-12-24 12:11:41 +0700

    add controls component

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import classes from './BuildControls.module.css'
 import BuildControl from './BuildControl/BuildControl'
 
+const controls = [
+  {label:'Salad', type: 'salad'},
+  {label:'Bacon', type: 'bacon'},
+  {label:'Cheese', type: 'cheese'},
+  {label:'Meat', type: 'meat'},
+]
+
 const BuildControls = props => {
   return (
     <div className={classes.BuildControls}>
-      <BuildControl />
+      <p>Current Price: $<strong>{props.total.toFixed(2)}</strong></p>
+      {controls.map(ctrl => (
+         <BuildControl label={ctrl.label}  key={ctrl.label} added={() => props.addingIngredient(ctrl.type)} removed={() => props.removingIngredient(ctrl.type)} disabled={props.disabled[ctrl.type]}/>
+      ))}
     </div>
   )
 }
